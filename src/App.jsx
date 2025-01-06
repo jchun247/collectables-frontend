c739e9f15c4546a6b289b2fc733980b0c7553ea7
Justin Chung <jchun247@uwo.ca>
2025-01-06 15:38:34 -0500

    Removed previous authentication logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import LoginPage from './pages/Login/Login'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { AuthProvider } from './auth/AuthProvider'
 
 const App = () => {
 
   return (
+    // <AuthProvider>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<AuthPage />}/>
+    //     </Routes>
+    //   </Router>
+    // </AuthProvider>
     <div>
-      <LoginPage />
+      Hello World!
     </div>
   )
 }
