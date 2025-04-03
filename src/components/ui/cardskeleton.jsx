7df29a5947f15edd284d6a5895dcbd51d69d1fd3
Justin Chung <jchun247@uwo.ca>
2025-04-03 14:21:28 -0400

    Update LoadingCardGrid and CardSkeleton to be consistent with RenderCard

--- src/components/ui/cardskeleton.jsx
+++ src/components/ui/cardskeleton.jsx
@@ -1,28 +1,29 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const CardSkeleton = () => {
   return (
-    <Card>
-        <CardHeader className="space-y-2 pb-3">
-        <Skeleton className="h-6 w-3/4" />
-        <Skeleton className="h-4 w-1/2" />
-        </CardHeader>
-        <CardContent>
-        {/* Image skeleton */}
-        <Skeleton className="aspect-[3/4] w-full mb-4" />
-        {/* Details skeleton */}
-        <div className="space-y-4">
-            <div className="flex justify-between items-center">
-            <Skeleton className="h-4 w-1/4" />
-            <Skeleton className="h-4 w-1/4" />
-            </div>
-            <div className="flex justify-between items-center">
-            <Skeleton className="h-4 w-1/3" />
-            <Skeleton className="h-4 w-1/4" />
-            </div>
+    <Card className="relative overflow-hidden">
+      <CardContent className="p-0">
+        <div className="relative rounded-lg overflow-hidden bg-muted/10">
+          <div className="aspect-[2.5/3.5]">
+            <Skeleton className="absolute inset-0 w-full h-full" />
+          </div>
         </div>
-        </CardContent>
+        <div className="p-4 h-[140px]">
+          <div className="h-full flex flex-col justify-between">
+            <div>
+              <Skeleton className="h-6 w-3/4 mb-2" />
+              <Skeleton className="h-4 w-1/3 mb-1" />
+              <Skeleton className="h-4 w-2/3" />
+            </div>
+            <div className="flex items-center justify-between mt-auto">
+              <Skeleton className="h-5 w-16" />
+              <Skeleton className="h-6 w-16" />
+            </div>
+          </div>
+        </div>
+      </CardContent>
     </Card>
   );
 }
