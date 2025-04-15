378bb68ebf6d341ae940b5e932a78c8ab3530c71
Justin Chung <jchun247@uwo.ca>
2025-04-14 21:09:11 -0400

    Display energy images in card details

---
+++ src/utils/energyTypeImages.js
@@ -0,0 +1,15 @@
+const energyTypeImages = {
+  GRASS: "https://site.pkmn.gg/images/types/grass-energy.png",
+  FIRE: "https://site.pkmn.gg/images/types/fire-energy.png",
+  WATER: "https://site.pkmn.gg/images/types/water-energy.png",
+  LIGHTNING: "https://site.pkmn.gg/images/types/lightning-energy.png",
+  FIGHTING: "https://site.pkmn.gg/images/types/fighting-energy.png",
+  PSYCHIC: "https://site.pkmn.gg/images/types/psychic-energy.png",
+  COLORLESS: "https://site.pkmn.gg/images/types/colorless-energy.png",
+  DARKNESS: "https://site.pkmn.gg/images/types/darkness-energy.png",
+  METAL: "https://site.pkmn.gg/images/types/metal-energy.png",
+  DRAGON: "https://site.pkmn.gg/images/types/dragon-energy.png",
+  FAIRY: "https://site.pkmn.gg/images/types/fairy-energy.png"
+};
+
+export default energyTypeImages;
